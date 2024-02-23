50fd2f36692e7b1c693bb5b6171e265f1f227d43
Yaojie Lu <yaojie.lu@cutlergrouplp.com>
2024-02-23 14:59:29 -0800

    123

--- parser.sv
+++ parser.sv
@@ -47,7 +47,7 @@ module parser(clk, reset_b, dataIn, dataIn_val, dataIn_ready, dataIN_last, //rec
     for (genvar i = 0; i < 10; i = i+1) begin
         assign dataOut[i*32:i*32+31] = outputFinal[i];
     end
-    assign dataout[288:295] = outputFinal[9][31:24];
+    assign dataOut[288:295] = outputFinal[9][31:24];
 
     always @ (posedge clk)
         if (!reset_b) begin
