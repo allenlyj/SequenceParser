dcc3e250533024bea21376471cf11fb1ba051917
Yaojie Lu <yaojie.lu@cutlergrouplp.com>
2024-02-23 20:48:57 -0800

    I

--- run.tcl
+++ run.tcl
@@ -1,11 +1,12 @@
 transcript file "./history.log"
 
 vlog -work work ./parser.sv
-vlog -work work ./tb.sv
+vlog -work work +acc=rnpbc ./tb.sv
 
 vsim -t 1ps -L work tb_parser
 
 config wave -signalnamewidth 1
 add wave -group DUT /tb_parser/*
 
+do wave.do
 run 20 us
